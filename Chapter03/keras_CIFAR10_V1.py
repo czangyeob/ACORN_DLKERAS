c21ec1c58e3d2402c934226487de66d2b1d54110
czangyeob <czangyeob@gmail.com>
2017-09-02 01:05:59 +0900

    Initial Commit

--- Chapter03/keras_CIFAR10_V1.py
+++ Chapter03/keras_CIFAR10_V1.py
@@ -51,7 +51,7 @@ model.add(Dropout(0.25))
 
 model.add(Conv2D(64, kernel_size=3, padding='same'))
 model.add(Activation('relu'))
-model.add(Conv2D(64, 3, 3))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
