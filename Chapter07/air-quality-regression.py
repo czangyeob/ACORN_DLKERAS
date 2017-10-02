618f76eb458e2fbac0a3bbc39beed95c091a0f08
czangyeob <czangyeob@gmail.com>
2017-10-02 17:35:26 +0900

    Initial Commit

--- Chapter07/air-quality-regression.py
+++ Chapter07/air-quality-regression.py
@@ -14,12 +14,12 @@ DATA_DIR = "../data"
 AIRQUALITY_FILE = os.path.join(DATA_DIR, "AirQualityUCI.csv")
 
 aqdf = pd.read_csv(AIRQUALITY_FILE, sep=";", decimal=",", header=0)
-# remove first and last 2 cols 
-del aqdf["#Date"]
+# 가장 앞 2열과 가장 뒤 2열 삭제
+del aqdf["Date"]
 del aqdf["Time"]
 del aqdf["Unnamed: 15"]
 del aqdf["Unnamed: 16"]
-# fill NaNs in each column with the mean value
+# 각 열의 NaN 값 들을 평균으로 채워 넣는다.
 aqdf = aqdf.fillna(aqdf.mean())
 # aqdf.head()
 
@@ -41,7 +41,7 @@ train_size = int(0.7 * X.shape[0])
 Xtrain, Xtest, ytrain, ytest = X[0:train_size], X[train_size:], y[0:train_size], y[train_size:]
 print(Xtrain.shape, Xtest.shape, ytrain.shape, ytest.shape)
 
-# define the network
+# 네트워크 정의
 readings = Input(shape=(12,))
 x = Dense(8, activation="relu", kernel_initializer="glorot_uniform")(readings)
 benzene = Dense(1, kernel_initializer="glorot_uniform")(x)
@@ -49,21 +49,21 @@ benzene = Dense(1, kernel_initializer="glorot_uniform")(x)
 model = Model(inputs=[readings], outputs=[benzene])
 model.compile(loss="mse", optimizer="adam")
 
-# train network
+# 네트워크 학습
 NUM_EPOCHS = 20
 BATCH_SIZE = 10
 
 history = model.fit(Xtrain, ytrain, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,
                    validation_split=0.2)
 
-# show some predictions
+# 일부 예측 값 시각화
 ytest_ = model.predict(Xtest).flatten()
 for i in range(10):
     label = (ytest[i] * Xstds[3]) + Xmeans[3]
     prediction = (ytest_[i] * Xstds[3]) + Xmeans[3]
     print("Benzene Conc. expected: {:.3f}, predicted: {:.3f}".format(label, prediction))
     
-# plot all predictions
+# 모든 예측 시각화
 plt.plot(np.arange(ytest.shape[0]), (ytest * Xstds[3]) / Xmeans[3], 
          color="b", label="actual")
 plt.plot(np.arange(ytest_.shape[0]), (ytest_ * Xstds[3]) / Xmeans[3], 
